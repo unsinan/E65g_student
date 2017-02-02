1b4b27e152d74ed6c99e7406ab9c6ea5073e7505
Unsin An <unsin.an@verizon.com>
2017-02-01 21:35:19 -0500

    assignment-1-submit

--- Assignment1/Assignment1/SecondViewController.swift
+++ Assignment1/Assignment1/SecondViewController.swift
@@ -10,9 +10,15 @@ import UIKit
 
 class SecondViewController: UIViewController {
 
+    @IBOutlet weak var gridSize: UISlider!
+    @IBOutlet weak var frameSecond: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        gridSize.transform = CGAffineTransform (rotationAngle: CGFloat(-M_PI_2))
+        frameSecond.transform = CGAffineTransform (rotationAngle: CGFloat(-M_PI_2))
     }
 
     override func didReceiveMemoryWarning() {
